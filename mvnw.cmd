c3d917e88cecda49fedbb97c5febdab0561b0a06
藏书君 <gg_happy@163.com>
2026-05-10 13:45:33 +0800

    init

--- mvnw.cmd
+++ mvnw.cmd
@@ -27,8 +27,6 @@
 @REM   MVNW_VERBOSE - true: enable verbose log; others: silence the output
 @REM ----------------------------------------------------------------------------
 
-set JAVA_HOME=D:\\opt\\jdk-21
-
 @IF "%__MVNW_ARG0_NAME__%"=="" (SET __MVNW_ARG0_NAME__=%~nx0)
 @SET __MVNW_CMD__=
 @SET __MVNW_ERROR__=
